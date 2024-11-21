168df493614440445147f0b06f9717e0434eda6a
enorapreault <enora.preault@ratp.fr>
2024-11-21 15:55:26 +0000

    wip : add langchain openai connection

---
+++ src/langchain_openai.py
@@ -0,0 +1,49 @@
+import openai
+import json
+import os
+from langchain.chat_models import AzureChatOpenAI
+from langchain.schema import HumanMessage
+
+from langchain import LLMChain
+from langchain import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Appeler API du LLM 
+
+openai_api_base=os.getenv('AZURE_OPENAI_ENDPOINT')
+openai_api_version=os.getenv('AZURE_OPENAI_API_VERSION')
+deployment_name=os.getenv('AZURE_OPENAI_MODELS')
+openai_api_key=os.getenv("AZURE_OPENAI_API_KEY")
+openai_api_type="azure"
+
+# Initialisation du LLM
+
+llm = AzureChatOpenAI(
+    #openai_api_base=openai_api_base,
+    #openai_api_version=openai_api_version,
+    #deployment_name=deployment_name,
+    #openai_api_key=openai_api_key,
+    #openai_api_type=openai_api_type
+)
+reponse = llm([HumanMessage(content="Write me a poem")])
+
+print(reponse)
+
+# Prompt pour le LLM
+
+template = """
+
+
+"""
+
+prompt = PromptTemplate(
+input_variables=["product_type", "customer_request"],
+template=template,
+)
+
+llm([HumanMessage(content="Write me a poem")])
+
+
+print()
