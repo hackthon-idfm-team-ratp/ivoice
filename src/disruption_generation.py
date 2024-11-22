fec52cd23edd99568ba2cb97b2cc2b5e9ada4fc2
Anand Badrinath <anand.badrinath@ratp.fr>
2024-11-22 09:56:10 +0000

    feat: Integrate itinerary generation

--- src/disruption_generation.py
+++ src/disruption_generation.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 
 def create_disruption():
-    csv_path = './Caspar/historique_disruptions_2024.csv'
+    csv_path = './Caspar/historique_disruptions_2024_ligne9.csv'
     df = pd.read_csv(csv_path)
     df_metro_9 = df[df['impacted_objects_pt_object_name'] == 'RATP Métro 9']
     df_metro_9_noservice = df_metro_9[df_metro_9['severity.effect'] == 'NO_SERVICE']
@@ -34,7 +34,7 @@ def create_disruption():
     severity_effect = severity_effect_list[random.randint(0,len(severity_effect_list)-1)]
     messages_text = messages_text_list[random.randint(0,len(messages_text_list)-1)]
     impacted_objects_pt_object_id = impacted_objects_pt_object_id_list[random.randint(0,len(impacted_objects_pt_object_id_list)-1)]
-        
+
 
     dico = {
         'date_fichier': '/',
@@ -58,10 +58,10 @@ def create_disruption():
     }
 
     cles_a_conserver =['period_begin','status','severity.effect','messages_text','impacted_objects_pt_object_id']
- 
+
     dico_filtre = {key : dico[key] for key in cles_a_conserver if key in dico}
-    
+
     if dico_filtre.get('status') == 'active' and dico_filtre.get('severity.effect') == 'NO_SERVICE':
         disruption = itinerary_data.Disruption(dico_filtre.get('period_begin'), dico_filtre.get('status'), dico_filtre.get('severity.effect'), dico_filtre.get('messages_text'), dico_filtre.get('impacted_objects_pt_object_id'))
-    
+
     return disruption
--- src/front.py
+++ src/front.py
@@ -1,55 +1,62 @@
 import streamlit as st
 import disruption_generation
 import time
+from itinerary_calculator import ItineraryCalculator
+from itinerary_data import Itinerary
+from dataclasses import asdict
 
 st.markdown("###")
 
 st.title("IVoice")
 
 ### Fake functions:
-def message_generation():
+def message_generation(itineraries: dict[str, list[Itinerary]]):
+    it_dict = {k: [asdict(it) for it in v] for k, v in itineraries.items()}
+    time.sleep(3)
     dico = {
         'Pont de sèvres': 'Prenez la ligne 8 pour aller à République',
         'Réaumur-Sébastopol': "Pour aller vers l'ouest prenez la 3",
         "Nation": "Pour aller vers l'ouest prenez la 1 vers l'ouest"
     }
-    time.sleep(3)
     return dico
 
-def itineraire_generation():
-    time.sleep(5)
 
 ###
+def itineraire_generation():
+    it_calc = ItineraryCalculator()
+    itineraries = it_calc.compute_alternate_itineraries(line_id="IDFM:C01379")
+    return itineraries
 
 def generation_incident():
     result = disruption_generation.create_disruption()
     st.markdown(result.message, unsafe_allow_html=True)
 
-def loading_itineraire():
+def loading_itineraire() -> dict[str, list[Itinerary]]:
     with st.spinner("Génération de l'itinéraire:"):
-        itineraire_generation()
+        itinerary = itineraire_generation()
     st.success("Itinéraire généré !")
+    return itinerary
 
-def loading_message():
+def loading_message(itinerary: dict[str, list[Itinerary]]) -> dict[str, str]:
     with st.spinner("Génération du message"):
-        message_generation()
+        messages = message_generation(itinerary)
     st.success("Message généré !")
+    return messages
 
-def print_messages():
-    dico = message_generation()
-    for key in dico.keys():
-        result = f"{key}: {dico.get(key)}"
+def print_messages(messages: dict[str, str]):
+    for key in messages.keys():
+        result = f"{key}: {messages.get(key)}"
         st.write(result)
 
 
 def print_results():
     generation_incident()
     st.write("\n")
-    loading_itineraire()
+    itinerary = loading_itineraire()
     st.write("\n")
-    loading_message()
+    messages = loading_message(itinerary)
     st.write("\n")
-    print_messages()
+    print_messages(messages)
 
 
 with st.container():
